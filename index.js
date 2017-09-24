a3e226d7c052830b6443c4f165be5159ba160b74
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-24 13:47:23 -0400

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,60 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {}
+  newItem[item]= Math.floor(Math.random() * 100) + 1
+  cart.push(newItem)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else if (cart.length === 1) {
+    var firstItem = Object.keys(cart[0])[0]
+    console.log(`In your cart, you have ${firstItem} at $${cart[0][firstItem]}.`)
+  } else if (cart.length === 2){
+    var firstItem = Object.keys(cart[0])[0]
+    var secondItem = Object.keys(cart[1])[0]
+    console.log(`In your cart, you have ${firstItem} at $${cart[0][firstItem]} and ${secondItem} at $${cart[1][secondItem]}.`)
+  } else {
+    var beginSentence = "In your cart, you have "
+    var listOfItems = []
+    for (let i=0; i < cart.length-1; i++){
+      var item = Object.keys(cart[i])[0]
+      listOfItems.push(`${item} at $${cart[i][item]}, `)
+    }
+    var lastItem = Object.keys(cart[cart.length-1])[0]
+    var lastItemString = `and ${lastItem} at $${cart[cart.length-1][lastItem]}.`
+    console.log(beginSentence + listOfItems.join("") + lastItemString)
+  }
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (let i=0; i < cart.length; i++ ){
+    var item = Object.keys(cart[i])[0]
+    sum+=cart[i][item]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i=0; i < cart.length; i++){
+    if (item === Object.keys(cart[i])[0]){
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
